f120e2f7609a9b468bd5e162a8f244706e6a4044
marcos-wesley <marcos.wesley@hotmail.com.br>
2025-08-21 21:11:08 -0300

    Atualização

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import symbolOn from './assets/pictures/symbol-on.png'
 import symbolOff from './assets/pictures/symbol-off.png'
 
 export default function App() {
-  const isActive = true;
+  const  [isActive, setisActive] = useState(false)
 
   function handleSymbol(){
-    console.log('Olá')
-  }
+
+    setisActive((oldValue:boolean) => {
+      return !oldValue
+  })
+
+  console.log(isActive)
+}
 
   return (
     <View style={isActive ? styles.containerOn : styles.containerOff }>
